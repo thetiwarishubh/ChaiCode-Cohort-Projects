4383d962cce017f27d8dea6c04fc11e19ec175ab
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-09-11 23:59:08 +0530

    complete 2 dom project

--- JavaScript/DOM Project 1-5/challenge-2/app.js
+++ JavaScript/DOM Project 1-5/challenge-2/app.js
@@ -1,3 +1,13 @@
-/**
- * Write your challenge solution here
- */
+const mainHeading = document.getElementById("mainHeading");
+const buttons = document.querySelectorAll(".color-buttons button");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", function() {
+        let color = this.innerText.toLowerCase()
+        if(color === "reset"){
+            mainHeading.style.color = "";
+        } else {
+            mainHeading.style.color = color;
+        }
+    })
+})
