ec4e5d5d3d87338946b05d609648aeb85e9d018c
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-09-13 01:54:11 +0530

    complete dom 3 projects

--- JavaScript/DOM Project 1-5/challenge-3/app.js
+++ JavaScript/DOM Project 1-5/challenge-3/app.js
@@ -1,3 +1,22 @@
-/**
- * Write your challenge solution here
- */
+const nameInput = document.getElementById("nameInput");
+const jobInput = document.getElementById("jobInput");
+const ageInput = document.getElementById("ageInput");
+const bioInput = document.getElementById("bioInput");
+const nameDisplay = document.getElementById("nameDisplay");
+const jobDisplay = document.getElementById("jobDisplay");
+const ageDisplay = document.getElementById("ageDisplay");
+const bioDisplay = document.getElementById("bioDisplay");
+
+const updateValue = function(input, display){
+    input.addEventListener("input", (e)=> {
+        if(e.target.value !== ""){
+            display.textContent = e.target.value.trim()
+        } else {
+            display.textContent = `Not provided`
+        }
+    })
+}
+updateValue(nameInput, nameDisplay)
+updateValue(jobInput, jobDisplay)
+updateValue(ageInput, ageDisplay)
+updateValue(bioInput, bioDisplay)
