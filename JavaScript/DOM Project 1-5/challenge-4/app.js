aec79a53e06618e47f6e06e2916ce02f9acb0971
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-09-13 20:55:56 +0530

    update

--- JavaScript/DOM Project 1-5/challenge-4/app.js
+++ JavaScript/DOM Project 1-5/challenge-4/app.js
@@ -1,3 +1,13 @@
-/**
- * Write your challenge solution here
- */
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
+const totalTasks = document.getElementById("totalTasks");
+const completedTasks = document.getElementById("completedTasks");
+
+addBtn.addEventListener("click", ()=> {
+    const inputValue = input.value.trim();
+    const li = document.createElement("li");
+    li.textContent = inputValue;
+    taskList.appendChild(li);
+    input.value = ""
+})
