a5af0a71359daaaff940138ecee0c35ba6a2eb6e
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-09-14 18:15:39 +0530

    add some features in project 4

--- JavaScript/DOM Project 1-5/challenge-4/app.js
+++ JavaScript/DOM Project 1-5/challenge-4/app.js
@@ -3,11 +3,59 @@ const addBtn = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 const totalTasks = document.getElementById("totalTasks");
 const completedTasks = document.getElementById("completedTasks");
+const deleteButtons = document.querySelectorAll(".delete-button");
 
-addBtn.addEventListener("click", ()=> {
-    const inputValue = input.value.trim();
-    const li = document.createElement("li");
-    li.textContent = inputValue;
-    taskList.appendChild(li);
-    input.value = ""
-})
+const addTask = function () {
+  const inputValue = input.value.trim();
+  if(!inputValue) return alert(`Please enter a task`);
+
+  //Create li
+  const li = document.createElement("li");
+  li.setAttribute("class", "task-item");
+
+  //Create span for text
+  const span = document.createElement("span");
+  span.setAttribute("class", "task-text");
+
+  //Create checkbox for task complete or not
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("class", "complete-checkbox");
+
+  //Create delete button for remove the tasks
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("class", "delete-button");
+  deleteButton.textContent = "delete";
+  console.log(deleteButton);
+
+  const emptyTasklist = document.querySelector(".empty-list")
+  if(emptyTasklist){
+    emptyTasklist.remove();
+  }
+
+  li.append(checkBox);
+  li.append(span);
+  span.textContent = inputValue;
+  li.append(deleteButton);
+  taskList.appendChild(li);
+  input.value = "";
+};
+
+addBtn.addEventListener("click", addTask);
+
+const totalTasksCount = function(){
+  let count = 0;
+
+  return {
+    increment : function(){
+      count++;
+      return count;
+    },
+    decrement : function(){
+      count--;
+      return count;
+    }
+  }
+}
+
+const tasks = totalTasksCount()
