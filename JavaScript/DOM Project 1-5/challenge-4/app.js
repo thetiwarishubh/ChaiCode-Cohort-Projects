b45cac195d8829d80b6b04577bcaf7d056761f35
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-09-17 23:31:27 +0530

    a fresh start of DOM project 4

---
+++ JavaScript/DOM Project 1-5/challenge-4/app.js
@@ -0,0 +1,6 @@
+const inputTask = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addButton");
+const taskContainers = document.getElementById("taskList");
+const totalTasksCount = document.getElementById("totalTasks");
+const completedTasksCount = document.getElementById("completedTasks");
+
