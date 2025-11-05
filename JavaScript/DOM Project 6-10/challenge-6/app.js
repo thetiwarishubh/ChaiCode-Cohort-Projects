1e3b90877bf0667f382df001794f3e66e02cc5b0
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-05 12:07:46 +0530

    format the code

--- JavaScript/DOM Project 6-10/challenge-6/app.js
+++ JavaScript/DOM Project 6-10/challenge-6/app.js
@@ -1,31 +1,38 @@
-const clock = document.querySelector('.clock');
+const clock = document.querySelector(".clock");
 const date = document.querySelector(".date");
 const digitalClock = document.querySelector(".digital-clock");
-const hourArrow = document.querySelector('.hour');
-const minuteArrow = document.querySelector('.minute');
-const secondArrow = document.querySelector('.second');
+const hourArrow = document.querySelector(".hour");
+const minuteArrow = document.querySelector(".minute");
+const secondArrow = document.querySelector(".second");
 
+const timeContainer = function () {
+  const currentTime = new Date();
+  const hour = currentTime.getHours().toString().padStart(2, "0");
+  const minute = currentTime.getMinutes().toString().padStart(2, "0");
+  const second = currentTime.getSeconds().toString().padStart(2, "0");
+  digitalClock.textContent = `${hour}:${minute}:${second}`;
+};
+timeContainer();
+setInterval(() => {
+  timeContainer();
+}, 1000);
 
-const timeContainer = function(){
-    const currentTime = new Date();
-    const hour = currentTime.getHours().toString().padStart(2, "0");
-    const minute = currentTime.getMinutes().toString().padStart(2, "0");
-    const second = currentTime.getSeconds().toString().padStart(2, "0");
-    digitalClock.textContent = `${hour}:${minute}:${second}`;
-}
-timeContainer()
-setInterval(()=>{
-    timeContainer()
-}, 1000)
-
-const dateContainer = function(){
-    const currentDate = new Date();
-    const dayNumber = currentDate.getDay();
-    const dates = currentDate.getDate();
-    const month = currentDate.toLocaleString('default', {month : "long"});
-    const year = currentDate.getFullYear();
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const dayName = days[dayNumber];
-    date.textContent = `${dayName} ${dates} ${month} ${year}`
-}
-dateContainer()
+const dateContainer = function () {
+  const currentDate = new Date();
+  const dayNumber = currentDate.getDay();
+  const dates = currentDate.getDate();
+  const month = currentDate.toLocaleString("default", { month: "long" });
+  const year = currentDate.getFullYear();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayName = days[dayNumber];
+  date.textContent = `${dayName} ${dates} ${month} ${year}`;
+};
+dateContainer();
