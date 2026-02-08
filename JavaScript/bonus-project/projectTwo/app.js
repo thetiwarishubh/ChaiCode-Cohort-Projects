cd29e74267494dd84ba0d618f21a9ec6e51c830d
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-02-08 21:26:55 +0530

    wrapped in function

--- JavaScript/bonus-project/projectTwo/app.js
+++ JavaScript/bonus-project/projectTwo/app.js
@@ -2,7 +2,7 @@ const input = document.getElementById("seconds");
 const btn = document.querySelector("button");
 const display = document.querySelector(".display");
 
-btn.addEventListener("click", () => {
+const timerFunction = () => {
   let inputValue = input.value;
   if (inputValue === "") {
     alert(`Please Enter time in seconds`);
@@ -11,15 +11,17 @@ btn.addEventListener("click", () => {
       inputValue--;
       if (inputValue > 0) {
         display.textContent = `Time remaining : ${inputValue + 1}`;
-        btn.textContent = `Stop Countdown`
+        btn.textContent = `Stop Countdown`;
         input.value = "";
       } else {
-        display.textContent = `Countdown Complete!`;
+        display.textContent = `Timer up⏰!`;
         input.value = "";
-        setTimeout(()=> {
-            window.location.reload()
-        }, 5000)
+        setTimeout(() => {
+          window.location.reload();
+        }, 5000);
       }
     }, 1000);
   }
-});
+};
+
+btn.addEventListener("click", timerFunction);
